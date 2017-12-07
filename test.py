4ea3e120f905876969e7060ffb5a64c283917385
Jaon95 <jasonlmh0314@outlook.com>
2017-12-07 21:47:01 +0800

    开始学习mako

--- test.py
+++ test.py
@@ -1,36 +1,9 @@
-import requests
-from spiderStudy import Downloader
-import lxml.html
-import cssselect
+from jinja2 import FileSystemLoader, Environment
+from datetime import datetime
 
-EMAIL = 'jasonlmh0314@outlook.com'
-PASSWORD = 'webscraping0314'
-def parseForm(html):
-    tree = lxml.html.fromstring(html)
-    data = {}
-    for item in tree.cssselect('form input'):
-        if item.get('name'):
-            data[item.get('name')] = item.get('value')
-    return data
+loader = FileSystemLoader('F:\\pystudy\\FlaskStudy\\template')
+template = Environment(loader=loader).get_template('hello_macro.html')
 
-s =requests.Session()
-data = parseForm(s.get('http://example.webscraping.com/places/default/user/login').text)
-data['email'] = EMAIL
-data['password'] = PASSWORD
-for item in s.cookies.iterkeys():
-    print(item +': '+s.cookies[item])
-rep = s.post('http://example.webscraping.com/places/default/user/login', data = data)
-# rep = s.get('http://example.webscraping.com/places/default/edit/Afghanistan-1')
-# data = parseForm(rep.text)
-# print(data)
-for item in s.cookies.iterkeys():
-    print(item +': '+s.cookies[item])
-print(rep.url)
-rep = s.get('http://example.webscraping.com/places/default/edit/Afghanistan-1')
-data = parseForm(rep.text)
-print(data)
-data['population'] = int(data['population'])+1
-s.post('http://example.webscraping.com/places/default/edit/Afghanistan-1', data = data)
-rep = s.get('http://example.webscraping.com/places/default/edit/Afghanistan-1')
-data = parseForm(rep.text)
-print(data)
+print(template.render(time = datetime.now()))
+
+print(datetime.now())
