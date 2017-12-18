24529b2dddd448032219060ac79e7ea37cd03294
Jaon96 <jasonlmh0314@outlook.com>
2017-12-18 22:33:54 +0800

    删除文件

--- test.py
+++ test.py
@@ -1,9 +1,1 @@
-from jinja2 import FileSystemLoader, Environment
-from datetime import datetime
-
-loader = FileSystemLoader('F:\\pystudy\\FlaskStudy\\template')
-template = Environment(loader=loader).get_template('hello_macro.html')
-
-print(template.render(time = datetime.now()))
-
-print(datetime.now())
+from flask import Flask
