9868cdb38832e52986c864de1cb0c3ecfcb81202
Jaon <jasonlmh0314@outlook.com>
2017-12-26 15:31:03 +0800

    添加html数据处理器

--- requester/requester/handles.py
+++ requester/requester/handles.py
@@ -1,3 +1,17 @@
+import lxml
+
+
+
+def crawl_execute(session,downlaoder,seed_url,func):
+    urls = []
+    urls.extend(seed_url)
+
+    item=urls.pop()
+    while item:
+        resp = downlaoder(item[0])
+        newUrls = func(session, resp)
+        urls.extend(newUrls)
+    
 
 class Downloader(object):
     def __init__(self, session, headers):
@@ -6,6 +20,15 @@ class Downloader(object):
     
     def __call__(self, url, method, data =None):
         if method == 'POST':
-            return self.session.post(url, data=data).text
+            return self.session.post(url, data=data)
         if method == 'GET':
-            return self.session.get(url).text
+            return self.session.get(url)
+
+def handlerKNKC(httpsession, resp):
+    html = resp.text
+    htmltree = lxml.html.fromstring(html)
+    #sava the data
+    #code...
+
+    #返回
+    return []
--- requester/requester/test1.py
+++ requester/requester/test1.py
@@ -2,9 +2,17 @@ import requests
 import utils
 import handles
 
-downloadHtml = handles.Downloader(requests.Session(), utils.getHeaders('e:/headers.txt'))
+#
+httpSession = requests.Session()
+init_headers = utils.getHeaders('e:/headers.txt')
 
-print(downloadHtml('http://logistics-front.liangdawang.com/logistics-front/ownInventoryWeightController/showList.htm?bindType=1','POST', {
+#创建一个下载器
+downloadHtml = handles.Downloader(httpSession, init_headers)
+
+#创建一个list，存储下载的种子，以及请求种子的方式、可选的数据参数
+seed_url = [('http://logistics-front.liangdawang.com/logistics-front/ownInventoryWeightController/showList.htm?bindType=1','GET',None),]
+
+r = downloadHtml('http://logistics-front.liangdawang.com/logistics-front/ownInventoryWeightController/showList.htm?bindType=1','POST', {
     'row':10,
     'page':3,
     'searchValue':'',
@@ -13,4 +21,7 @@ print(downloadHtml('http://logistics-front.liangdawang.com/logistics-front/ownIn
     'startDate':'',
     'endDate':'',
     'outState':''
-}))
+})
+
+print(r.headers)
+print(r.request.headers)
