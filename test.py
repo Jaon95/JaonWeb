fa5ce0b016404fae9923f6fcd57eebf223c97b13
Jaon96 <jasonlmh0314@outlook.com>
2018-02-26 23:15:29 +0800

    extension notation

--- test.py
+++ test.py
@@ -1,25 +1,4 @@
-import lxml.html
+import re
 
-
-tree = lxml.html.fromstring("""
-     <html>
-        <body>
-            <div class='content'>
-                <table>
-                    <tr>
-                        <td>nihao:ddd</td>
-                        <td>dafad</td>
-                    </tr>
-                     <tr>
-                        <td>nihao:ddd</td>
-                        <td>dafad</td>
-                    </tr>                   
-                </table>
-            </div>
-        </body>
-     </html>
-""")
-
-elements = tree.cssselect('.content tr')
-for item in elements:
-    print(item.text_content())
+result = re.search(r'(?: [\w]+)','hello li')
+print(result.group())
